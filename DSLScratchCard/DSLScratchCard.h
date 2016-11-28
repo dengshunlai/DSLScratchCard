d441b9cf5567b95cf8047b31f450c787c3ccd508
dengshunlai <mu3305495@163.com>
2016-11-28 22:46:50 +0800

    create
    create

---
+++ DSLScratchCard/DSLScratchCard.h
@@ -0,0 +1,13 @@
+//
+//  DSLScratchCard.h
+//  DSLScratchCard
+//
+//  Created by 邓顺来 on 16/11/28.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSLScratchCard : UIView
+
+@end
---
+++ DSLScratchCard/DSLScratchCard.m
@@ -0,0 +1,21 @@
+//
+//  DSLScratchCard.m
+//  DSLScratchCard
+//
+//  Created by 邓顺来 on 16/11/28.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import "DSLScratchCard.h"
+
+@implementation DSLScratchCard
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
